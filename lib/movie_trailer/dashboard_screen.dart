b961a2e6e8aa904d8c03df7651c3fc62bb194d64
ahmedmohamed2030074 <wowrar1234@gmail.com>
2020-01-11 21:22:26 +0200

    EdgeInsets

---
+++ lib/movie_trailer/dashboard_screen.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+//    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Movie Trailers"),
+        centerTitle: false,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Container(
+        child: Center(
+//          child: Text("Hello"),
+
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text("Recommended",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            )),
+                        FlatButton(
+                          child: Text("View All"),
+                          onPressed: () {},
+                        )
+                      ],
+                    ),
+                    Container(
+//                  padding: const EdgeInsets.all(10),
+                      height: 250,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          Container(
+                            child: Padding(
+                              padding: const EdgeInsets.all(5),
+                              child: Column(
+                                children: <Widget>[
+                                  Container(
+//                                    padding: const EdgeInsets.all(10),
+//                            padding: EdgeInsets.all(10),
+//                          width: double.infinity,
+                                    width: 150,
+                                    height: 200,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        image: DecorationImage(
+                                          image: NetworkImage(
+                                              "https://moviehole.net/img/maxresdefault-74.jpg"),
+//                                fit: BoxFit.cover,
+                                          fit: BoxFit.fill,
+                                        )),
+                                  ),
+                                  Text(
+                                    "Love",
+                                    style: TextStyle(
+//                            fontWeight: FontWeight.bold,
+                                      fontWeight: FontWeight.normal,
+                                      //wordSpacing: 20,
+                                      letterSpacing: 0,
+                                      fontSize: 17,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: Padding(
+                              padding: const EdgeInsets.all(5),
+                              child: Column(
+                                children: <Widget>[
+                                  Container(
+//                                    padding: const EdgeInsets.all(10),
+//                            padding: EdgeInsets.all(10),
+//                          width: double.infinity,
+                                    width: 150,
+                                    height: 200,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        image: DecorationImage(
+                                          image: NetworkImage(
+                                              "https://moviehole.net/img/maxresdefault-74.jpg"),
+//                                fit: BoxFit.cover,
+                                          fit: BoxFit.fill,
+                                        )),
+                                  ),
+                                  Text(
+                                    "Love",
+                                    style: TextStyle(
+//                            fontWeight: FontWeight.bold,
+                                      fontWeight: FontWeight.normal,
+                                      //wordSpacing: 20,
+                                      letterSpacing: 0,
+                                      fontSize: 17,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: Padding(
+                              padding: const EdgeInsets.all(5),
+                              child: Column(
+                                children: <Widget>[
+                                  Container(
+//                                    padding: const EdgeInsets.all(10),
+//                            padding: EdgeInsets.all(10),
+//                          width: double.infinity,
+                                    width: 150,
+                                    height: 200,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        image: DecorationImage(
+                                          image: NetworkImage(
+                                              "https://moviehole.net/img/maxresdefault-74.jpg"),
+//                                fit: BoxFit.cover,
+                                          fit: BoxFit.fill,
+                                        )),
+                                  ),
+                                  Text(
+                                    "Love",
+                                    style: TextStyle(
+//                            fontWeight: FontWeight.bold,
+                                      fontWeight: FontWeight.normal,
+                                      //wordSpacing: 20,
+                                      letterSpacing: 0,
+                                      fontSize: 17,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/movie_trailer/main.dart
+++ lib/movie_trailer/main.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      centerTitle: true,
-      title: Text("Movie Trailer"),
-    ),
-  ),
-));
+import 'dashboard_screen.dart';
+
+//void main() => runApp(MaterialApp(
+//  home: Scaffold(
+//    appBar: AppBar(
+//      centerTitle: true,
+//      title: Text("Movie Trailer"),
+//    ),
+//    body: MovieTrailerDashBoard(),
+//
+//  ),
+//));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Movie Trailer",
+      theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white, primarySwatch: Colors.indigo),
+      home: DashboardScreen(),
+    );
+  }
+}
